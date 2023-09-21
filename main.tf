2263616107ea5e7309aa387d45b114d870c05e29
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-09-21 15:22:35 +0000

    #13 Add Random Terraform Provider

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
 
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+  
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+}
+
+
+output "random_bucket_name_result" {
+ value = random_string.bucket_name.result
+}
