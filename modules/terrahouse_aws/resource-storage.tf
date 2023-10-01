5978064477f07945da3cc19c9e7752fa8a134ed4
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-10-01 19:33:38 +0000

    #45 Assets path and script slash

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -35,7 +35,7 @@ resource "aws_s3_object" "upload_assets" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
   source = "${var.assets_path}/${each.key}"
-  etag = filemd5("${var.assets_path}${each.key}")
+  etag = filemd5("${var.assets_path}/${each.key}")
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes = [etag]
@@ -48,7 +48,7 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
   content_type = "text/html"
 
-  #etag = filemd5(var.error_html_filepath)
+  etag = filemd5(var.error_html_filepath)
   #lifecycle {
   #  ignore_changes = [etag]
   #}
