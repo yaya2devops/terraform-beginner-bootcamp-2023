cf6405b58541da099c5006449e5dce643752e5eb
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-10-05 14:37:59 +0000

    #53 Terratowns Custom Provider Block

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path  # Add this line
-}
+
+
+terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+  }
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#}
